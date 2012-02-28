27e5fdf489aa818d95806d29d7082df8a2af22a3
Joshua Smith <jsmith@addsheet.com>
2012-02-28 17:35:06 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FundrunnersWeb::Application.config.secret_token = '1a8e6419bed569df48e9da6db37c71d36bddb596e6951d8c048b74ed91f6ed4b5710f55231f60edd49bcd9bfa26bf2fc1b0b5f4f824d325ff18fcbbb88510d30'
